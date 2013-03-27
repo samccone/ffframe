7424d3eac67f67ffb9b14cc79ac29334ef99e96e
Sam Saccone <samccone@gmail.com>
2013-03-26 23:23:45 -0400

    swap mongodb for mongoose

--- db/interface.js
+++ db/interface.js
@@ -1,11 +1,24 @@
-var Db  = require('mongodb')
+var mongoose = require('mongoose');
 
 exports.connect = function(cb) {
-  Db.connect(process.env['db'] || 'mongodb://localhost/db', function(err, d) {
+  mongoose.connect(process.env['db'] || 'mongodb://localhost/db', function(err, d) {
     if (err) {
       console.log("º Error Connecting", JSON.stringify(err));
     } else {
       console.log("º DB Connected");
+      setSchemas()
     }
-  })
+  });
 }
+
+function setSchemas() {
+  var frameSchema = mongoose.Schema({
+    title: String,
+    url: String,
+    caption: String
+  });
+
+  var frame = mongoose.model('frame', frameSchema);
+}
+
+exports.inser
